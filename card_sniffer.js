f69cba9037c737f729abd4980b5e270f49ae4e2e
Tim Tilberg <ttilberg@gmail.com>
2020-11-16 11:21:44 -0600

    Improve docs and usage

--- card_sniffer.js
+++ card_sniffer.js
@@ -1,8 +1,17 @@
+////////////////////////////////////
+// Configuration!
+//
+// Your twitch username:
 const TWITCH_USER = 'USER';
+//
 // Get your oauth token by going here: https://twitchapps.com/tmi/
 const TWITCH_OATH = 'OAUTH_TOKEN';
+//
+// The channel -- probably the same as your username?
+const CHANNEL = 'That Channel';
+//
+////////////////////////////////////
 
-const channel = 'That Channel';
 
 const client = new tmi.Client({
   options: { debug: true, messagesLogLevel: "info" },
@@ -14,36 +23,34 @@ const client = new tmi.Client({
     username: TWITCH_USER,
     password: TWITCH_OATH
   },
-  channels: [ channel ]
+  channels: [ CHANNEL ]
 });
 client.connect().catch(console.error);
 
+chrome.webRequest.onCompleted.addListener(
+  (details) => {
+    const { url } = details
 
+    if (!url.match(/scryfall-cards.*jpg/)) return;
 
-(function() {
-  chrome.webRequest.onCompleted.addListener((details) => {
-    const {url} = details
-
-    if(!url.match(/scryfall-cards.*jpg/)) return;
     console.log('Someone found a card!');
 
-    const card_id = url.match( /\/([^/]+).jpg/ )[1];
+    const card_id = url.match(/\/([^/]+).jpg/)[1];
+
     console.log(card_id);
 
     fetch(`https://api.scryfall.com/cards/${card_id}`)
-    .then(response => response.json())
-    .then(data => {
-      let url = data.related_uris.gatherer;
-      let name = data.name;
-      const msg = `Card: [${name}](${url})`;
-      console.log(msg);
-      client.say(channel, msg);
-    });
-  },
-  {
+      .then(response => response.json())
+      .then(data => {
+        let url = data.related_uris.gatherer;
+        let name = data.name;
+        const msg = `reviewed [${name}](${url})`;
+        console.log(msg);
+        client.action(CHANNEL, msg);
+      });
+  }, {
     urls: [
-    'https://*.scryfall.com/*'
+      'https://*.scryfall.com/*'
     ]
   }
-  );
-})()
+);
--- readme.md
+++ readme.md
@@ -5,7 +5,17 @@ A friend and I play Magic on www.Spelltable.com. It's cool. We also wanted to li
 This hacky extension sniffs network traffic for "Hey, you tried looking at that card details" and forwards that information to a configured twitch chat.
 
 
+# The Tin
+
+Spelltable provides an interface to look up cards by either searching for card names, or clicking on a card in play. When you pull up a card, it makes a query to [Scryfall](https://scryfall.com/docs/api) to get the card image.
+
+This extension adds a hook for those image requests by watching for specific url patterns that signify an image load from Scryfall --> `url.match(/scryfall-cards.*jpg/)`. If we see this request, someone (probably) pulled up a card. We extract the card id from the image filename, and make a followup request to get card details. This gives us the name, and the Gatherer url.
+
+Finally, the [TMI Twitch Chat package](https://github.com/tmijs/tmi.js) makes short work of sending our message back to the configured Twitch chat.
+
+
 # Installation
+
 - Clone/download this repo somewhere memorable
 - Edit the file `card_sniffer.js` to include your correct user/oauth/channel credentials
 - Go to about://extensions in Chrome.
@@ -14,5 +24,6 @@ This hacky extension sniffs network traffic for "Hey, you tried looking at that 
 - Sling some spells
 
 
-# Password/Oauth
+## Password/Oauth
+
 Twitch chatbots require oauth. You can get an oauth token for your account at: https://twitchapps.com/tmi/
